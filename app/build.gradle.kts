55b2285296bd497988805a1326954ecd40842312
iamarjun <arjun.manoj@practo.com>
2021-05-03 12:41:14 +0530

    Add necessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -18,6 +18,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "BASE_URL", "\"https://cdn-api.co-vin.in/api/\"")
     }
 
     buildTypes {
@@ -29,7 +31,11 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -40,37 +46,43 @@ android {
 
 dependencies {
 
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:1.1.5")
+
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
 
-    implementation ("androidx.fragment:fragment-ktx:1.3.3")
-    implementation ("androidx.activity:activity-ktx:1.2.2")
+    implementation("androidx.fragment:fragment-ktx:1.3.3")
+    implementation("androidx.activity:activity-ktx:1.2.2")
 
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation ("androidx.lifecycle:lifecycle-common-java8:2.3.1")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0-alpha01")
 
     //WorkManager
-    implementation ("androidx.work:work-runtime-ktx:2.5.0")
+    implementation("androidx.work:work-runtime-ktx:2.5.0")
+    implementation("androidx.hilt:hilt-work:1.0.0-beta01")
+    kapt("androidx.hilt:hilt-compiler:1.0.0-beta01")
 
     //Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
 
     //Hilt
-    implementation ("com.google.dagger:hilt-android:2.35.1")
-    kapt( "com.google.dagger:hilt-compiler:2.35.1")
+    implementation("com.google.dagger:hilt-android:2.35.1")
+    kapt("com.google.dagger:hilt-compiler:2.35.1")
 
-    //Moshi
-    implementation ("com.squareup.moshi:moshi-kotlin:1.10.0")
-    kapt ("com.squareup.moshi:moshi-kotlin-codegen:1.10.0")
+    //GSON
+    implementation("com.google.code.gson:gson:2.8.6")
 
     //Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //DataStore
+    implementation("androidx.datastore:datastore-preferences:1.0.0-beta01")
 
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
